ab482a4051f355de69bf6a298e0b25b0baf61b95
liAmirali <liamirali.lotfi@gmail.com>
2020-05-15 00:45:14 +0430

    More specific logs

--- js/money.js
+++ js/money.js
@@ -1,22 +1,25 @@
-// Everybody pays tax to a random guy! (for no reason :D)
+// Everybody pays tax to a random guy! (for no reason :D) Is it socialism or what? :DD
 function payMoneyToEachOther(){
     for (var personId = 0; personId < peopleC; personId++) {
         if (world[personId].alive) {
+            let guy = findRandomGuy(personId);
             let tax = world[personId].generousness * 100;
             if (world[personId].money >= tax) {
                 world[personId].money -= tax;
-                world[findRandomGuy(personId)].money += tax;
+                world[guy].money += tax;
             }
             else{
                 let helpedAmount = askNeighbor(personId, 'money', tax);
                 if (helpedAmount === 0) {
+                    console.log("Neighbor \#" + whoIsNeighbor(personId) + " DIDN'T HELP person \#" + personId);
                     die(personId, 'poverty');
                 }
                 else{
-                    // These two following lines neutralizes each other, so it's better to comment them
+                    // These two following lines neutralizes each other (because helpedAmount = tax),
                     // world[personId].money += helpedAmount;
                     // world[personId].money -= tax;
-                    world[findRandomGuy(personId)].money += tax;
+                    world[guy].money += tax;
+                    console.log("Neighbor \#" + whoIsNeighbor(personId) + " GAVE $" + tax + " to person \#" + personId);
                 }
             }
         }
