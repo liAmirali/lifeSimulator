9f912183bf73d5caa0d4545f5a34acf2d9e96257
liAmirali <liamirali.lotfi@gmail.com>
2020-05-15 00:45:56 +0430

    Add a new function, checkSurvivor()

--- js/death.js
+++ js/death.js
@@ -1,4 +1,19 @@
 function die(personId, cause) {
-    console.log(`RIP.. :(. ${personId} died because of ${cause}.`);
+    console.log(`Person \#${personId} DIED because of ${cause}.`);
     world[personId].alive = false;
+}
+
+function checkSurvivor() {
+    if (countAlive() === 1) {
+        refreshStat();
+        let survivedGuy;
+        for (var i = 0; i < peopleC; i++) {
+            if (world[i].alive) { survivedGuy = i; break;}
+        }
+        alert("Person \#" + i + " is the only SURVIVOR from this world!!!");
+        return true;
+    }
+    else {
+        return false;
+    }
 }
