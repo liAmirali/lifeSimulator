10fd9ea932df664bf5485cb057622b4f60042678
Amirali <liamirali.lotfi@gmail.com>
2020-12-15 17:45:23 +0330

    Deleted js version

--- py.v/randwrld.py
+++
@@ -1,8 +0,0 @@
-from main import *
-from random import randint
-
-def randperson(personHimself):
-    randwid = randint(0, peopleC-1)
-    if randwid == personHimself or not world[randwid].alive:
-        randperson(personHimself)
-    return randwid
