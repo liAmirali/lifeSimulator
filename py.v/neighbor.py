c3328d42701d766ffe58fc9a1e31f6e8baac4775
Amirali <liamirali.lotfi@gmail.com>
2020-12-15 17:37:31 +0330

    Debugged ask()

--- py.v/neighbor.py
+++ py.v/neighbor.py
@@ -13,11 +13,23 @@ def whoIs(wid):
             i += 1
     return neighborwid
 
+# Messed up code!
 def ask(wid, reason, askedAmount):
     neighborwid = whoIs(wid)
+    neighborAsset = None
+    if reason == 'food' :
+        neighborAsset = world[neighborwid].food
+    elif reason == 'money':
+        neighborAsset = world[neighborwid].money
+    else:
+        print("REASON NOT FOUND in neighbor.ask(). --->", reason)
+
     maxPossibleHelp = world[neighborwid].generosity
-    if maxPossibleHelp >= askedAmount:
-        world[neighborwid][reason] -= askedAmount
+    if maxPossibleHelp >= askedAmount and neighborAsset >= askedAmount:
+        if reason == 'food' :
+            world[neighborwid].food -= askedAmount
+        elif reason == 'money':
+            world[neighborwid].money -= askedAmount
         return askedAmount
     else:
         return False
